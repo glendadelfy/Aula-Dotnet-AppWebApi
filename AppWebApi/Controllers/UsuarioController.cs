74ec19c98271278780a6a047521acc6a07e18cb0
glenda <glendadelfy20@gmail.com>
2024-10-21 00:09:54 -0300

    Aula online .NET AppWebApi 08/10/2024

---
+++ AppWebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,67 @@
+﻿using AppWebApi.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AppWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public IActionResult Get(int id)
+        {
+            var user = new UsuarioModel().MockList();
+            if (user == null)
+
+                return NotFound();
+
+            return Ok(user);
+
+        }
+        [HttpPost]
+        [Route("Adicionar")]
+        public IActionResult Post([FromBody] UsuarioModel model) {
+            {
+                if (model == null)
+                    return BadRequest();
+
+                var user = new UsuarioModel()
+                {
+                    id = model.id,
+                    nome = model.nome,
+                    dataNascimento = model.dataNascimento,
+                    endereco = model.endereco,
+                    email = model.email,
+
+                };
+                return Ok(user);
+            }
+
+        }
+
+        [HttpPut]
+        [Route("Atualizar")]
+        public IActionResult Put([FromBody] UsuarioModel model)
+        {
+            if (model == null)
+                return BadRequest();
+            var userResponse = new UsuarioModel().MockUpdate(model);
+            if (userResponse.id == 0)
+                return BadRequest();
+            return Ok(userResponse);
+        }
+
+        [HttpDelete]
+        [Route("Deletar")]
+        public IActionResult Delete(long id)
+        {
+            if (id == 0)
+                return BadRequest();
+            return NoContent();
+        }
+    }
+
+}
---
+++ AppWebApi/Model/UsuarioModel.cs
@@ -0,0 +1,37 @@
+﻿namespace AppWebApi.Model
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string email { get; set; } 
+        public string endereco { get; set; }
+
+
+        public UsuarioModel MockList()
+        {
+            var user = new UsuarioModel()
+            {
+                id = 1,
+                nome = "Glenda Delfy",
+                dataNascimento = new DateTime(2004,08,15),
+                email = "glendadelfy20@gmail.com",
+                endereco = "Rua abc123"
+            };
+            return user;
+        }
+        public UsuarioModel MockUpdate(UsuarioModel _user)
+        {
+            if (_user.id == 1 )
+            {
+                return _user;
+
+            }
+            else
+            {
+                return new UsuarioModel();
+            }
+        }
+    }
+}
